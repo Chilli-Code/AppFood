07dcef6e061097fab7baf3774f5068fb3f125896
Chilli-Code <jorgexc42@gmail.com>
2025-07-15 00:23:27 -0500

    Prueba conexion base de datos Menu

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+
+// import seed from '@/lib/seed'; // Adjust the import path as necessary
+//import { Button, Text } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+// <Button title="Search" onPress={() => seed().catch((error) => console.log('Faild to seed the database', error))}  />
+import { getCategories, getMenu } from "@/lib/appwrite";
+import useAppwrite from '@/lib/useAppwrite';
+import { useLocalSearchParams } from 'expo-router';
+import { useEffect } from 'react';
 
 const Search = () => {
+  const { category, query } = useLocalSearchParams<{query: string; category: string}>();
+
+  const { data, refetch, loading } = useAppwrite({
+    fn:getMenu,
+    params: {
+      category,
+      query, 
+      limit: 6,
+    },
+  })
+
+  const { data: categories } = useAppwrite({fn: getCategories});
+
+  useEffect( () =>{
+    refetch({ category, query, limit: 6 });
+  }, [category, query]);
+
+  console.log(data);
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <FlatList data={data} renderItem={({ item, index }) => {
+        return(
+          <View className='flex-1 max-w-[48%]'>
+            <Text>Menu Card</Text>
+          </View>
+        )
+      }} />
+    </SafeAreaView>
   )
 }
 
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,12 +1,18 @@
-import { CreateUserParams, SignInParams } from "@/type";
-import { Account, Avatars, Client, Databases, ID, Query } from "react-native-appwrite";
+import { CreateUserParams, GetMenuParams, SignInParams } from "@/type";
+import { Account, Avatars, Client, Databases, ID, Query, Storage } from "react-native-appwrite";
 
 export const appwriteConfig ={
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
     platform: "com.chilly.foodapp",
     databaseId: "6872b2e50017bc9ea520",
+    bucketId: "6875c328002b8d93b571",
     userCollectionId: "6872b307000c15867fc0",
+    categoriesCollectionId: "6875be4600071ed59a05",
+    menuCollectionId: "6875bf03002eddab92b2",
+    customizationsCollectionId: "6875c08c0003daba4a54",
+    menuCustomizationsCollectionId: "6875c1d300308c344a50",
+
 }
 
 
@@ -19,30 +25,27 @@ client
 
 
 export const account = new Account(client);
-export const database = new Databases(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
 const avatars = new Avatars(client); 
 
 
-export const createUser = async ({email, password, name}: CreateUserParams) => {
-    try{
-
+export const createUser = async ({ email, password, name }: CreateUserParams) => {
+    try {
         const newAccount = await account.create(ID.unique(), email, password, name)
-
         if(!newAccount) throw Error;
 
-        await signIn({email, password});
+        await signIn({ email, password });
 
         const avatarUrl = avatars.getInitialsURL(name);
 
-        return await database.createDocument(
+        return await databases.createDocument(
             appwriteConfig.databaseId,
             appwriteConfig.userCollectionId,
             ID.unique(),
-            { email, name, accountId: newAccount.$id, avatar:avatarUrl }
+            { email, name, accountId: newAccount.$id, avatar: avatarUrl }
         );
-
-
-    }catch(e){
+    } catch (e) {
         throw new Error(e as string);
     }
 }
@@ -60,7 +63,7 @@ export const getCurrentUser = async () =>{
         const currentAccount = await account.get();
         if(!currentAccount) throw Error;
 
-        const currentUser = await database.listDocuments(
+        const currentUser = await databases.listDocuments(
             appwriteConfig.databaseId,
             appwriteConfig.userCollectionId,
             [Query.equal('accountId', currentAccount.$id)]
@@ -75,3 +78,34 @@ export const getCurrentUser = async () =>{
     }
 }
 
+
+export const getMenu = async({ category, query }: GetMenuParams) => {
+    try{
+        const queries: string [] = [];
+
+        if(category) queries.push(Query.equal('categories', category));
+        if(query) queries.push(Query.search('name', query)); 
+
+        const menus = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.menuCollectionId,
+            queries,
+        )
+        return menus.documents;
+
+    }catch(e){
+        throw new Error (e as string);
+    }
+}
+
+export const getCategories = async () => {
+    try{
+        const categories = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.categoriesCollectionId,
+        )
+
+    }catch(e){
+        throw new Error(e as string);
+    }
+}
